8c6c38424a92151fee07ba93809a6d11eaaa3b57
Opemipo Disu <46439130+coderoflagos@users.noreply.github.com>
2022-07-24 17:28:05 +0100

    commit

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,8 +5,8 @@ import Toggle from '../components/toggle';
 const Navbar = () => {
   return (
     <header>
-     <div className='text-black flex justify-between mb-5 items-center h-full max-w-[1240px] mx-auto px-4'>
-        <h1 className="w-full text-3xl font-bold text-black dark:text-white">BLOG.</h1>          
+     <div className='text-black flex justify-between mb-5 items-center max-w-[1240px] mx-auto px-4'>
+        <h1 className="w-full text-3xl font-bold text-black dark:text-white">PP & PB </h1>          
         <div className="p-4 flex">
           <Toggle />
         </div>
